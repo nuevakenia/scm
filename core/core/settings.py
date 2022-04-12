38d30d2e0a3337ad2574bf9b06aed4589d6002f9
nuevakenia <daniel.galli.lillo@gmail.com>
2022-04-12 16:29:09 -0400

    d

--- core/core/settings.py
+++ core/core/settings.py
@@ -139,6 +139,6 @@ REST_FRAMEWORK = {
 }
 
 CORS_ALLOWED_ORIGINS = [
-    "http://localhost:8001",
+    "http://127.0.0.1:8001",
     "http://localhost:8002",
 ]
--- externo/enrolamiento/static/js/enrolamiento.js
+++ externo/enrolamiento/static/js/enrolamiento.js
@@ -1,5 +1,5 @@
 console.log("Pruebaa")
-
+const csrftoken = Cookies.get("csrftoken");
 function encodeImageFileAsURL(element) {
     let file = element.files[0];
     let reader = new FileReader();
@@ -13,45 +13,42 @@ function encodeImageFileAsURL(element) {
 
 // Porcentaje Total 
 const enrolamiento = function () {
-    foto = document.getElementById("imagen");
-    enconde = encodeImageFileAsURL(foto);
-    fetch(
-  
-        `http://127.0.0.1:8000/`,
-        {
-            method: "POST",
-            body: JSON.stringify({
-                enconde,
-                
-            }),
-            headers: {
-                "content-type": "application/json",
-                
-                "X-CSRFToken": csrftoken,
-            },
-        }
-    ).then(async (response) => {
-        const textoRespuesta = await response.json();
-        if (response.status === 400) {
-            if (textoRespuesta.codigo[0].includes("enrolamiento")) {
-                
-                Swal.fire(
-                    "Cancelado",
-                    "Error",
-                    "error"
-                );
-            } else {
-              
-                toastr.error(
-                    "Ha ocurrido un inconveniente, intente más tarde"
-                );
-            }
-        } else if (response.ok) {
-            console.log("Enrolamiento Exitoso")
-        } else {
-            toastr.error(
-                "Ha ocurrido un inconveniente, intente más tarde"
-            );
-        }
-    });
+    let encode = "";
+    let foto = document.getElementById("imagen");
+    //encode = encodeImageFileAsURL(foto);
+    let dispositivo = 1
+    let usuario = 1
+    let ingreso = "2022-04-27 23:03:00-04"
+    let salida = "2022-04-28 21:03:00-05"
+    var url = 'http://127.0.0.1:8000/api/ingreso/crear';
+
+    const formData = new FormData();
+
+    const file = $("#imagen")[0].files[0];
+    documento = $("#imagen")[0].files[0],file.fileName;
+        formData.append('imagen', file);
+        formData.append('empleado', usuario);
+        formData.append('dispositivo', dispositivo);
+        formData.append('ingreso', ingreso);
+        formData.append('salida', salida);
+
+        console.log("AAAAAAAAAA DOCUMENTO");
+        console.log($("#imagen")[0].files[0],file.fileName);
+
+fetch(url, {
+  method: 'POST', // or 'PUT'
+  body: formData,// data can be `string` or {object}!
+  headers:{
+    "X-CSRFToken": csrftoken,
+  }
+}).then(res => res.json())
+.catch(error => console.error('Error:', error))
+.then(response => console.log('Success:', response));
+
     }
+    
+
+    let btn = document.getElementById("enviarImagen");
+btn.addEventListener('click', event => {
+    enrolamiento();
+});
